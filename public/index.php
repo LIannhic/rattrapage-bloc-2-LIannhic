b861db352d0741da243c221fb7a1ffb5ee22cacd
LIannhic <154102382+LIannhic@users.noreply.github.com>
2025-10-06 11:53:27 +0200

    Feat: implémentation de la récupération des catégories depuis la base de données

--- public/index.php
+++ public/index.php
@@ -7,7 +7,10 @@ require_once __DIR__ . '/../config/functions.php';
 // TODO ... 
 
 // Récupération des catégories depuis la base de données
-// TODO ... 
+$sql = "SELECT * FROM categories";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Affichage de la page d'accueil
 require_once __DIR__ . '/../templates/index.html.php';
