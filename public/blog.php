e6f5fcbeb95657bf3bc3545e459ccc2fb4e709be
LIannhic <154102382+LIannhic@users.noreply.github.com>
2025-10-06 13:40:15 +0200

    Feat: Ajout connexion à la base de donnée et implémentation de la récupération des articles depuis la base de donnée

--- public/blog.php
+++ public/blog.php
@@ -1,8 +1,12 @@
 <?php
-
+require_once __DIR__ . '/../config/db_connect.php';
+require_once __DIR__ . '/../config/functions.php';
 
 // Récupération des articles depuis la base de données
-// TODO ... 
+$sql = "SELECT * FROM articles";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Récupération des catégories depuis la base de données
 // TODO ...
